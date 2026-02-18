c860bc75f341d55834d46544c11f371dd6c6cc15
Alex Harvey <alex.harvey@workwave.com>
2026-02-18 17:07:29 +1100

    release: v0.1.1

--- Casks/logs-parser.rb
+++ Casks/logs-parser.rb
@@ -1,6 +1,6 @@
 cask "logs-parser" do
-  version "0.1.0"
-  sha256 "d9e4bd064f88e1a8cdd594e4ddcddbffbca5de67a8607a500b1189195c4f8373"
+  version "0.1.1"
+  sha256 "27250930d7fe785d2ee1de7138ba3b5a6a422fe072b83711574c160a5ff92c61"
 
   url "https://github.com/harveyalex/logs-parser/releases/download/v#{version}/logs-parser.dmg"
   name "Logs Parser"
