4fd661fecfde88f3d0bcbeb797937db417944548
harveyalex <108037960+harveyalex@users.noreply.github.com>
2026-03-02 00:05:05 +0000

    chore: update logs-parser to 0.2.0

--- Casks/logs-parser.rb
+++ Casks/logs-parser.rb
@@ -1,6 +1,6 @@
 cask "logs-parser" do
-  version "0.1.3"
-  sha256 "c613909b738f985a8330fa76d514593a02cc3c3e6c64727556f8b38f2e5279de"
+  version "0.2.0"
+  sha256 "d8ecdc37a789e15a4b35f45ac2af0b3b3f83c6c39462afd849b337d4f0af8733"
 
   url "https://github.com/harveyalex/logs-parser/releases/download/v#{version}/logs-parser.dmg"
   name "Logs Parser"
